55ccb45f73d8172c09b326929f002bf712925e90
Omar Herri <herri.omar@gmail.com>
2021-03-23 13:49:26 +0100

    wip

--- src/Rules/RecaptchaRule.php
+++ src/Rules/RecaptchaRule.php
@@ -3,13 +3,14 @@
 
 namespace Combindma\Recaptcha\Rules;
 
+use Combindma\Recaptcha\Recaptcha;
 use Illuminate\Contracts\Validation\Rule;
 
 class RecaptchaRule implements Rule
 {
     public function passes($attribute, $value)
     {
-        return recaptcha()->validate($value)['success'];
+        return (new Recaptcha())->validate($value)['success'];
     }
 
     public function message()
