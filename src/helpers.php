fd3e1bbd0b20f90cd3c7147d11eb547858d6d75d
Omar Herri <herri.omar@gmail.com>
2021-03-23 13:30:40 +0100

    wip

--- src/helpers.php
+++ src/helpers.php
@@ -1,22 +1,15 @@
 <?php
 
-if (! function_exists('recaptcha')) {
-    function recaptcha(): \Combindma\Recaptcha\Recaptcha
-    {
-        return app('recaptcha');
-    }
-}
-
 if (! function_exists('htmlScriptTagJsApi')) {
     function htmlScriptTagJsApi(?array $config = []): string
     {
-        return recaptcha()->htmlScriptTagJsApi($config);
+        return (new \Combindma\Recaptcha\Recaptcha())->htmlScriptTagJsApi($config);
     }
 }
 
 if (! function_exists('recaptchaInput')) {
     function recaptchaInput(): string
     {
-        return recaptcha()->recaptchaInput();
+        return (new \Combindma\Recaptcha\Recaptcha())->recaptchaInput();
     }
 }
