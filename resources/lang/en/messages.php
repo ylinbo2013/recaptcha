adf0f6c7c1219a11d1ac738063828c981b866e77
Omar Herri <herri.omar@gmail.com>
2021-04-03 18:46:12 +0100

    wip

--- resources/lang/en/messages.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'invalid' => 'We could not verify if you are a robot or not. Please refresh the page.',
-];
--- resources/lang/fr/messages.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'invalid' => 'Nous n\'avons pas pu vérifier si vous êtes un robot ou pas. Veuillez actualiser la page.',
-];
